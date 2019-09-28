76ffb4102a2684c0f129b3d9f8bffbc18009109a
MrEliptik <victormeunier.dev@gmail.com>
2019-09-28 16:56:40 +0200

    add mask_image function

--- shotty.py
+++ shotty.py
@@ -7,6 +7,53 @@ import numpy as np
 import signal
 import cv2 as cv
 
+def mask_image(imgdata, imgtype='jpg', size=64):
+    """Return a ``QPixmap`` from *imgdata* masked with a smooth circle.
+
+    *imgdata* are the raw image bytes, *imgtype* denotes the image type.
+
+    The returned image will have a size of *size* × *size* pixels.
+
+    """
+    # Load image and convert to 32-bit ARGB (adds an alpha channel):
+    image = QImage.fromData(imgdata, imgtype)
+    image.convertToFormat(QImage.Format_ARGB32)
+
+    # Crop image to a square:
+    imgsize = min(image.width(), image.height())
+    rect = QRect(
+        (image.width() - imgsize) / 2,
+        (image.height() - imgsize) / 2,
+        imgsize,
+        imgsize,
+    )
+    image = image.copy(rect)
+
+    # Create the output image with the same dimensions and an alpha channel
+    # and make it completely transparent:
+    out_img = QImage(imgsize, imgsize, QImage.Format_ARGB32)
+    out_img.fill(Qt.transparent)
+
+    # Create a texture brush and paint a circle with the original image onto
+    # the output image:
+    brush = QBrush(image)        # Create texture brush
+    painter = QPainter(out_img)  # Paint the output image
+    painter.setBrush(brush)      # Use the image texture brush
+    painter.setPen(Qt.NoPen)     # Don't draw an outline
+    painter.setRenderHint(QPainter.Antialiasing, True)  # Use AA
+    painter.drawEllipse(0, 0, imgsize, imgsize)  # Actually draw the circle
+    painter.end()                # We are done (segfault if you forget this)
+
+    # Convert the image to a pixmap and rescale it.  Take pixel ratio into
+    # account to get a sharp image on retina displays:
+    pr = QWindow().devicePixelRatio()
+    pm = QPixmap.fromImage(out_img)
+    pm.setDevicePixelRatio(pr)
+    size *= pr
+    pm = pm.scaled(size, size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+
+    return pm
+
 def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
@@ -48,7 +95,8 @@ def main():
     label.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowType_Mask)
     monitor = QDesktopWidget().screenGeometry(0)
     label.move(monitor.left(), monitor.top())
-    label.showFullScreen()
+    #label.showFullScreen()
+    label.show()
 
     sys.exit(app.exec_())
 
