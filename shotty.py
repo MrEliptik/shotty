dc405f7cefdc3c5a90b6909af23b0e5ec9daacc3
Victor Meunier <mreliptik@pop-os.localdomain>
2019-09-29 09:08:22 +0200

    put app inside class

--- shotty.py
+++ shotty.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QLabel, QDesktopWidget
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage, QPixmap
 import sys
@@ -7,50 +7,57 @@ import numpy as np
 import signal
 import cv2 as cv
 
-def main():
+class Shotty(QWidget):
+    def __init__(self, im):
+        super().__init__()
+        print('Removing alpha..')
+        self.im = im[:,:,:3].copy()
+        self.initUI()
+        self.setMouseTracking(True)
+
+    def initUI(self):
+        QApplication.setOverrideCursor(Qt.CrossCursor)
+        # Create widget
+        self.label = QLabel(self)
+        self.l_mousePos = QLabel(self)
+        h, w, c = self.im.shape
+        print(h, w ,c)
+
+        
+        self.im = self.im[:,:,:3].copy()
+        h, w, c = self.im.shape
+        print('New shape: {},{},{}'.format(h, w, c))
+
+        
+        print(self.im.strides[0])
+        qImg = QImage(self.im, w, h, QImage.Format_RGB888).rgbSwapped()
+        pixmap = QPixmap.fromImage(qImg)
+        self.label.setPixmap(pixmap)
+        self.label.resize(pixmap.width(), pixmap.height())
+
+        self.label.keyPressEvent = app.quit()      
+
+        self.label.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowType_Mask)
+        monitor = QDesktopWidget().screenGeometry(1)
+        self.label.move(monitor.left(), monitor.top())
+        self.label.showFullScreen()
+    
+    def mouseMoveEvent(self, event):
+        print(event.x(), event.y())
+        self.l_mousePos.setText('(%dpx, %dpx)' % (event.x(), event.y()))
+
+
+if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     with mss.mss() as sct:
         # Get raw pixels from the screen, save it to a Numpy array
         im = np.array(sct.grab(sct.monitors[1]))
-    
+    '''
     cv.imshow("screenshot", im)
     cv.waitKey(0)
     cv.destroyAllWindows()
-    
-    
-
+    '''
     app = QApplication(sys.argv)
-
-    # Create widget
-    label = QLabel()
-    label2 = QLabel()
-    h, w, c = im.shape
-    print(h, w ,c)
-
-    print('Removing alpha..')
-    im = im[:,:,:3].copy()
-    h, w, c = im.shape
-    print('New shape: {},{},{}'.format(h, w, c))
-
-    
-    print(im.strides[0])
-    qImg = QImage(im, w, h, QImage.Format_RGB888).rgbSwapped()
-
-    #qImg = QImage(np.require(im, np.uint8, 'C'), w, h, im.strides[0], QImage.Format_RGB888)
-    pixmap = QPixmap.fromImage(qImg)
-    label.setPixmap(pixmap)
-    label.resize(pixmap.width(), pixmap.height())
-
-    label.keyPressEvent = app.quit()
-     
-
-    label.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowType_Mask)
-    monitor = QDesktopWidget().screenGeometry(0)
-    label.move(monitor.left(), monitor.top())
-    label.showFullScreen()
-
+    shotty = Shotty(im)
     sys.exit(app.exec_())
-
-if __name__ == "__main__":
-    main()
