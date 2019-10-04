01ac7fe072a13b1db35bd0bada03312dd80d569d
Victor Meunier <mreliptik@pop-os.localdomain>
2019-10-05 00:43:53 +0200

    test for system tray

---
+++ test.py
@@ -0,0 +1,20 @@
+from PyQt5 import Qt
+import sys
+app = Qt.QApplication(sys.argv)
+
+systemtray_icon = Qt.QSystemTrayIcon(Qt.QIcon('icons/screenshot.png'), app)
+
+if systemtray_icon.isSystemTrayAvailable():
+    # Context Menu
+    ctmenu = QMenu()
+    actionshow = ctmenu.addAction("Show/Hide")
+    actionshow.triggered.connect(
+        lambda: self.hide() if self.isVisible() else self.show())
+    actionquit = ctmenu.addAction("Quit")
+    actionquit.triggered.connect(self.close)
+
+    systemtray_icon.setContextMenu(ctmenu)
+    systemtray_icon.show()
+    systemtray_icon.showMessage('Title', 'Content')
+else:
+    print("NOT SUPPORTED")
