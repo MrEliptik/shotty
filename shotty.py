0f3f729a2b2c4707cd8bba5ec2c64581557b61b2
mreliptik <victormeunier.dev@gmail.com>
2019-10-04 22:57:03 +0200

    add platform check and pyhook

--- shotty.py
+++ shotty.py
@@ -4,11 +4,36 @@ from PyQt5.QtGui import QImage, QPixmap, QPalette, QPainter, QBrush, QColor, QPe
 import sys
 import mss
 import numpy as np
-import signal
-import cv2 as cv
-
-import pyxhook
+import platform
 import time
+
+_platform = platform.system()
+
+if _platform == 'Linux':
+    import pyxhook
+
+    # This function is called every time a key is presssed
+    def kbevent(event):
+        global running
+        # print key info
+        print(event)
+
+        # If the ascii value matches spacebar, terminate the while loop
+        if event.Ascii == 44:
+            running = False
+
+elif _platform == 'Windows':
+    import pythoncom
+    from pyHook import HookManager, GetKeyState, HookConstants
+
+    def OnKeyboardEvent(event):
+        print(repr(event), event.KeyID, HookConstants.IDToName(event.KeyID), event.ScanCode , event.Ascii, event.flags)
+        if event.KeyID == 44:
+            print("snapshot pressed")
+        return True
+
+elif _platform == 'Darwin':
+    print('[ERROR] macOS not supported!')
 
 
 def MouseMoveFilter(QObject):
@@ -350,32 +375,31 @@ def mask_image(imgdata, imgtype='jpg', size=64):
 
     return pm
 
-# This function is called every time a key is presssed
-def kbevent(event):
-    global running
-    # print key info
-    print(event)
-
-    # If the ascii value matches spacebar, terminate the while loop
-    if event.Ascii == 32:
-        running = False
-
 def main():
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    if _platform == 'Linux':
+        # Create hookmanager
+        hookman = pyxhook.HookManager()
+        # Define our callback to fire when a key is pressed down
+        hookman.KeyDown = kbevent
+        # Hook the keyboard
+        hookman.HookKeyboard()
+        # Start our listener
+        hookman.start()
 
-    # Create hookmanager
-    hookman = pyxhook.HookManager()
-    # Define our callback to fire when a key is pressed down
-    hookman.KeyDown = kbevent
-    # Hook the keyboard
-    hookman.HookKeyboard()
-    # Start our listener
-    hookman.start()
-
-    # Create a loop to keep the application running
-    running = True
-    while running:
-        time.sleep(0.1)
+        # Create a loop to keep the application running
+        running = True
+        while running:
+            time.sleep(0.1)
+    
+    elif _platform == 'Windows':
+        # create a hook manager
+        hm = HookManager()
+        # watch for all mouse events
+        hm.KeyDown = OnKeyboardEvent
+        # set the hook
+        hm.HookKeyboard()
+        # wait forever
+        pythoncom.PumpMessages()
 
     with mss.mss() as sct:
         # Get raw pixels from the screen, save it to a Numpy array
@@ -386,8 +410,10 @@ def main():
 
     sys.exit(app.exec_())
 
+    '''
     # Close the listener when we are done
     hookman.cancel()
+    '''
 
 if __name__ == "__main__":
     main()
